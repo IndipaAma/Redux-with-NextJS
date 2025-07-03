23da97b05accd6d1514b30176e23caeb1b0e4780
IndipaAma <amanethindipa123@gmail.com>
2025-07-03 14:47:16 +0530

    added styling

--- app/components/AgePredict.tsx
+++ app/components/AgePredict.tsx
@@ -24,23 +24,34 @@ const AgePredict = () => {
   };
 
   return (
-    <div className="@container">
-      <div className="flex justify-center  @sm:@max-md:flex-col">
+    <div className="">
+      <div className="flex justify-center my-5 ">
         <input
           type="text"
           placeholder="Enter Name"
           value={name}
           onChange={(e) => setName(String(e.target.value))}
-          className=""
+          className="border-1 rounded-md mx-3 p-1"
         />
-        <button onClick={handlePrediction} className="">
+        <button
+          onClick={handlePrediction}
+          className="bg-emerald-300 py-3 px-10 rounded-md"
+        >
           Predict
         </button>
       </div>
-      <div>
-        <h1>Age: {age}</h1>
-        <h1>Gender:{gender}</h1>
-        <h1>Country:{country}</h1>
+      <div className="flex flex-col text-center py-5 gap-2">
+        <h1 className="text-2xl font-semibold">
+          Age: <span className="text-lg">{age}</span>
+        </h1>
+        <h1 className="text-2xl font-semibold">
+          Gender:
+          <span className="text-lg">{gender}</span>
+        </h1>
+        <h1 className="text-2xl font-semibold">
+          Country:
+          <span className="text-lg">{country}</span>
+        </h1>
       </div>
     </div>
   );
